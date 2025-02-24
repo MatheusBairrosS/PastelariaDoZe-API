c45c2f24a1242e44aa5f6d1d5fb0a81cc79871e4
MatheusBairrosS <matheusbs123zt@gmail.com>
2025-02-24 20:39:31 -0300

    feat: Criação do banco

--- src/app/ClienteDAO.py
+++ src/app/ClienteDAO.py
@@ -7,21 +7,21 @@ router = APIRouter()
 # Criar os endpoints de Cliente: GET, POST, PUT, DELETE
 
 @router.get("/cliente/", tags=["Cliente"])
-def get_cliente():
+async def get_cliente():
     return {"msg": "get todos executado"}, 200
 
 @router.get("/cliente/{id}", tags=["Cliente"])
-def get_cliente(id: int):
+async def get_cliente(id: int):
     return {"msg": "get um executado"}, 200
 
 @router.post("/cliente/", tags=["Cliente"])
-def post_cliente(corpo: Cliente):
+async def post_cliente(corpo: Cliente):
     return {"msg": "post executado"}, 200
 
 @router.put("/cliente/{id}", tags=["Cliente"])
-def put_cliente(id: int):
+async def put_cliente(id: int):
     return {"msg": "put executado"}, 200
 
 @router.delete("/cliente/{id}", tags=["Cliente"])
-def delete_cliente(id: int):
+async def delete_cliente(id: int):
     return {"msg": "delete executado"}, 200
--- src/app/ProdutoDAO.py
+++ src/app/ProdutoDAO.py
@@ -7,21 +7,21 @@ router = APIRouter()
 # Criar os endpoints de Cliente: GET, POST, PUT, DELETE
 
 @router.get("/produto/", tags=["Produto"])
-def get_produto():
+async def get_produto():
     return {"msg": "get todos executado"}, 200
 
 @router.get("/produto/{id}", tags=["Produto"])
-def get_produto(id: int):
+async def get_produto(id: int):
     return {"msg": "get um executado"}, 200
 
 @router.post("/produto/", tags=["Produto"])
-def post_produto(corpo: Produto):
+async def post_produto(corpo: Produto):
     return {"msg": "post executado"}, 200
 
 @router.put("/produto/{id}", tags=["Produto"])
-def put_produto(id: int):
+async def put_produto(id: int):
     return {"msg": "put executado"}, 200
 
 @router.delete("/produto/{id}", tags=["Produto"])
-def delete_produto(id: int):
+async def delete_produto(id: int):
     return {"msg": "delete executado"}, 200
